035e52b84202e229059f7a4cae0d641545fabb8a
Julianna <julianna@Juliannas-MacBook-Pro.local>
2025-08-09 22:24:38 -0700

    webpages are now connected, buttons updated

--- myscripts.js
+++ myscripts.js
@@ -1,17 +1,27 @@
-const check = document.getElementById('check');
-const xmark = document.getElementById('xmark');
+document.addEventListener("DOMContentLoaded", () => {
+  const check = document.getElementById('check');
+  const xmark = document.getElementById('xmark');
+  const main_title = document.getElementById('main_title');
+  const back = document.getElementById('back');
 
-function selectImage(selected, other) {
-  // Add outline class to selected image
-  selected.classList.add('outlined');
-  // Remove outline class from the other image
-  other.classList.remove('outlined');
-}
+  function selectImage(selected, other) {
+    selected.classList.add('outlined');
+    other.classList.remove('outlined');
+  }
 
-check.addEventListener('click', () => {
-  selectImage(check, xmark);
-});
+  check.addEventListener('click', () => {
+    selectImage(check, xmark);
+  });
 
-xmark.addEventListener('click', () => {
-  selectImage(xmark, check);
+  xmark.addEventListener('click', () => {
+    selectImage(xmark, check);
+  });
+
+  main_title.addEventListener("click", function () {
+    window.location.href = "credits.html";
+  });
+
+  back.addEventListener("click", function () {
+    window.location.href = "main.html";
+  });
 });
