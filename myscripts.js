54089ceeb3b5b5b3f36d6c0594f2561c29f47282
Julianna <julianna@Juliannas-MacBook-Pro.local>
2025-08-09 20:20:02 -0700

    100% complete main.html

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,17 @@
-document.getElementById("infoBtn").addEventListener("click", function() {
-    const text = document.getElementById("infoText");
-    text.style.display = (text.style.display === "none") ? "block" : "none";
-  });
+const check = document.getElementById('check');
+const xmark = document.getElementById('xmark');
+
+function selectImage(selected, other) {
+  // Add outline class to selected image
+  selected.classList.add('outlined');
+  // Remove outline class from the other image
+  other.classList.remove('outlined');
+}
+
+check.addEventListener('click', () => {
+  selectImage(check, xmark);
+});
+
+xmark.addEventListener('click', () => {
+  selectImage(xmark, check);
+});
