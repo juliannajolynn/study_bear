5525d14be47e9678eb6c73a80e76fa3e97b6a3dd
Julianna <julianna@Juliannas-MacBook-Pro.local>
2025-08-09 17:39:17 -0700

    83%

---
+++ myscripts.js
@@ -0,0 +1,4 @@
+document.getElementById("infoBtn").addEventListener("click", function() {
+    const text = document.getElementById("infoText");
+    text.style.display = (text.style.display === "none") ? "block" : "none";
+  });
