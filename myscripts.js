3454085cc128741b540f662341fe62e2bfe4f89d
juliannajolynn <julianna.jolynn@berkeley.edu>
2026-03-08 21:22:08 -0700

    ok

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
-  var modal = document.getElementById("myModal");
-
-  modal.style.display = "block";
 
 
   const check = document.getElementById('check');
@@ -100,19 +96,5 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "timer.html";
   });
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 });
